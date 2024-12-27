db161e3de55ec0695c6f00e596ad8340f0333194
My-desiny-ae <tonnyk1130@gmail.com>
2024-12-27 19:25:27 +0900

    단어 혼용한되게 조심하기

--- todo-back/server.js
+++ todo-back/server.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 5001;
 
-app.use(cors());
-app.use(bodyParser.json());
+//데이터 저장용 배열
+const messages = [];
 
-app.get('/', (req, res)=>{
-    res.send("sever is running!");
+app.use(cors());
+app.use(express.json());
+
+//POST 요청으로 데이터 저장
+app.post('/api/send', (req, res)=>{
+    const {text} = req.body;
+    console.log("Received text from React : ", text);
+    messages.push(text); // 배열에 데이터 저장
+    res.json({messages : messages})
+});
+
+//GET 요청으로 데이터 변환
+app.get('/api/messages', (req, res) => {
+    res.json({ messages : messages}); //저장된 데이터 반환
 });
 
 app.listen(PORT, ()=>{
-    console.log('Sever is running on http//:localhost:${PORT}');
+    console.log(`Sever is running on http://localhost:${PORT}`);
 });
 
-app.get('/api/message', (req, res) => {
-    res.json({ message: 'Hello from the backend!' });
-});
+
--- todo-front/src/App.js
+++ todo-front/src/App.js
@@ -1,20 +1,48 @@
 import React, {useState, useEffect} from 'react';
 
 function App() {
-  const [message, setMessage] = useState(''); 
+  const [inputText, setInputText] = useState('');
+  const [messages, setMessages] = useState([]); //서버에서 가져온 메시지
 
-  useEffect(()=>{
-    //백엔드 api 호출
-    fetch("http://localhost:5001/api/message")
+
+  useEffect(() => {
+    fetch('http://localhost:5001/api/messages')
       .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error('Error fetch message', error));
-  }, [])
+      .then((data) => setMessages(data.messages))
+      .catch((error) => console.error('Error fetching messages : ', error));
+  }, []);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:5001/api/send', {
+      method : 'POST', 
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body : JSON.stringify({text : inputText}),
+    })
+      .then(() => {
+        fetch('http://localhost:5001/api/messages')
+          .then((response) => response.json())
+          .then((data) => setMessages(data.messages));
+        setInputText('');
+      })
+      .catch((error) => console.error('error sending data : ', error));
+  };
+  
 
   return (
     <div>
-      <h1>react와 express연결</h1>
-      <p>{message}</p>
+      <h1>React 와 express 데이터 전송</h1>
+      <form onSubmit = {handleSubmit}>
+        <input type='text' value={inputText} onChange={(e)=> setInputText(e.target.value)} placeholder='텍스트 입력'/>
+        <button type='submit'>전송</button>
+      </form>
+      <h2>메시지 목록</h2>
+      <ol>
+        {messages.map((msg, index) => (<li key={index}>{msg}</li>))}
+      </ol>
     </div>
   );
 }

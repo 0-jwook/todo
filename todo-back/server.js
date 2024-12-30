4695b548f701b07e0d157c2a7dd413c19bbb2e5c
My-desiny-ae <tonnyk1130@gmail.com>
2024-12-31 08:30:19 +0900

    DELETE

--- todo-back/server.js
+++ todo-back/server.js
@@ -23,6 +23,31 @@ app.get('/api/messages', (req, res) => {
     res.json({ messages : messages}); //저장된 데이터 반환
 });
 
+app.delete('/api/messages/:id', (req, res) => {
+    const { id } = req.params;
+    const index = parseInt(id, 10);
+    if (index >= 0 && index < messages.length){
+        messages.splice(index, 1);
+        res.json({success : true})
+    }else{
+        res.status(400).json({success : false, message : "Invalid ID"});
+    }
+});
+
+app.put('api/messages/:id', (req, res) => {
+    const { id } = req.params; // URL에서 id 추출
+    const { text } = req.body; // 요청 바디에서 수정할 텍스트 추출
+    const index = parseInt(id, 10); // 문자열을 숫자로 변환
+
+    if (index >= 0 && index<messages.length){
+        messages[index] = text;// 배열 데이터 수정
+        res.json({success : true, messages});// 성공 시 업데이트된 데이터 반환
+    }else{
+        res.status(400).json({ success: false, message: "Invalid ID" }); // 실패 시 에러 메시지
+    }
+    
+})
+
 app.listen(PORT, ()=>{
     console.log(`Sever is running on http://localhost:${PORT}`);
 });
